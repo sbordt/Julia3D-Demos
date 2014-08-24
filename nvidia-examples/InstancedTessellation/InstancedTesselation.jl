9b674ff15ed1f2e6ecae343d7815c70356555bbf
sbordt <sebastian.bordt@gmail.com>
2014-08-25 01:16:22 +0200

    : sbo
    Signed-off-by: sbordt <sebastian.bordt@gmail.com>

--- nvidia-examples/InstancedTessellation/InstancedTesselation.jl
+++ nvidia-examples/InstancedTessellation/InstancedTesselation.jl
@@ -3,20 +3,34 @@ using GLPlot #toopengl
 
 include("../../include.jl")
 
-window = createwindow("Mesh Display", 1000, 1000, debugging = false)
+window = createwindow("Mesh Display", 1000, 1000, debugging = true)
 cam = PerspectiveCamera(window.inputs, Vec3(2,2,0.5), Vec3(0))
 
-@time a = importOBJ("assets/models/airboat.obj", faceindextype=GLuint, vertextype=Float32)
-
+# vs_compiled, nvs_compiled, uvs_compiled, fcs_compiled, groups, smoothing_groups, mtllibs, materials
+@time obj = importOBJ("assets/models/Butterfly/Butterfly.obj", faceindextype=GLuint, vertextype=Float32)
+@time computeNormals!(obj)
+@time lol = compile(obj)
+(vs, nvs, uvs,  vs_material_id, fcs) = lol
+println(map(length,lol))
 #@time mtl = importMTL("assets/models/Butterfly/Butterfly.mtl", colortype=Float32)
 #println(length(mtl))
 
 shader = TemplateProgram("assets/shaders/standard.vert", "assets/shaders/phongblinn.frag")
-
+println(minimum(vs_material_id))
+println(maximum(vs_material_id))
+matid = GLBuffer(vs_material_id, 1)
+println(eltype(matid))
 data = [
-	:vertex 		=> GLBuffer(unitGeometry(a[1])),
-#b	:normal			=> GLBuffer(a[2]),
-	:indexes		=> indexbuffer(a[4]),
+	:vertex 		=> GLBuffer(unitGeometry(vs)),
+	:normal			=> GLBuffer(nvs),
+	:uv				=> GLBuffer(uvs),
+	:material_id	=> matid,
+	:indexes		=> indexbuffer(fcs),
+
+	:tex1 			=> Texture("assets/models/Butterfly/BUTTBODY.JPG"),
+	:tex2 			=> Texture("assets/models/Butterfly/BUTTFACE.JPG"),
+	:tex3 			=> Texture("assets/models/Butterfly/BUTTWIBA.JPG"),
+	:tex4 			=> Texture("assets/models/Butterfly/BUTTWIFR.JPG"),
 
 	:view 			=> cam.view,
 	:projection 	=> cam.projection,
